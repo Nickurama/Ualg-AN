c39366f93ab43dcac74289d38402fb8c7a726c25
Nickurama <rokdoombeast@gmail.com>
2023-10-26 11:02:25 +0100

    fixed some bugs

--- RootFinder/usingSymPy/math_input.py
+++ RootFinder/usingSymPy/math_input.py
@@ -1,17 +1,18 @@
 from sympy import *
 
+
 class math_input:
     @staticmethod
     def get_precision():
         this_input = input("precision: ")
         try:
             try:
-                this_input = N(this_input)
+                this_input = int(this_input)
             except:
                 raise Exception("Error: precision must be an integer.")
-            if (ask(Q.integer(this_input))):
+            if not ask(Q.integer(this_input)):
                 raise Exception("Error: precision must be an integer.")
-            if (this_input < 0 or this_input > 100):
+            if this_input < 0 or this_input > 100:
                 raise Exception("Error: Precision must be between 0 and 100.")
             return int(this_input)
         except Exception as e:
@@ -24,18 +25,18 @@ class math_input:
             return N(this_input, precision)
         except:
             raise Exception("Error: Invalid expression: '" + this_input + "'.")
-    
+
     @staticmethod
     def get_range0(precision):
         this_input = input("initial range: ")
         try:
             this_input = N(this_input, precision)
-            if (not ask(Q.real(this_input))):
+            if not ask(Q.real(this_input)):
                 raise Exception()
             return this_input
         except:
             raise Exception("Error: Invalid range.")
-        
+
     @staticmethod
     def get_range1(range0, precision):
         this_input = input("final range: ")
@@ -44,32 +45,38 @@ class math_input:
         except:
             raise Exception("Error: Invalid range.")
         try:
-            if (not ask(Q.real(this_input))):
+            if not ask(Q.real(this_input)):
                 raise Exception("Error: Invalid range.")
-            elif (range0 >= this_input):
-                raise Exception("Error: final range should be greater than initial range.")
+            elif range0 >= this_input:
+                raise Exception(
+                    "Error: final range should be greater than initial range."
+                )
             return this_input
         except Exception as e:
             raise e
-    
+
     @staticmethod
     def get_error_margin(precision):
         this_input = input("error margin: ")
         try:
             this_input = N(this_input, precision)
-            if (not ask(Q.real(this_input))):
+            if not ask(Q.real(this_input)):
                 raise Exception("Error: Invalid error margin.")
+            if ask(Q.negative(this_input)):
+                raise Exception("Error: Margin should be positive.")
             return this_input
         except Exception as e:
             raise e
-    
+
     @staticmethod
     def get_max_iterations(precision):
         this_input = input("max iterations: ")
         try:
             this_input = N(this_input, precision)
-            if (not ask(Q.real(this_input)) and not ask(Q.positive_infinite(this_input))):
+            if not ask(Q.real(this_input)) and not ask(Q.positive_infinite(this_input)):
                 raise Exception("Error: Invalid maximum iterations.")
+            if ask(Q.negative(this_input)):
+                raise Exception("Error: iterations should be positive.")
             return this_input
         except Exception as e:
             raise e
