33ace0243e7ea9eeb3e1c5b144a3d76c5d8da80d
Nickurama <rokdoombeast@gmail.com>
2023-11-01 21:13:52 +0000

    main done

--- Matrices/main.py
+++ Matrices/main.py
@@ -20,6 +20,19 @@ while exit == false:
         pprint(MatMul(matrix, equalities_matrix), use_unicode=False)
         error_margin = math_input.math_input.get_error_margin(precision)
         max_iterations = math_input.math_input.get_max_iterations(precision)
+        try:
+            print(">-- solve (Jacobi) --<")
+            result_jacobi = jacobi.jacobi.solve(matrix, equalities, error_margin, max_iterations)
+            print(f"result: {round(result_jacobi, precision)}")
+        except:
+            print("Jacobi's method failed.")
+        try:
+            print(">-- solve (Gauss) --<")
+            result_gauss = gauss.gauss.solve(matrix, equalities, error_margin, max_iterations)
+            print(f"result: {round(result_gauss, precision)}")
+        except Exception as e:
+            print(str(e))
+            print("Gauss's method failed.")
     except KeyboardInterrupt:
         exit = true
     except Exception as e:
