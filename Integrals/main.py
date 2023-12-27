7896eb83f272c1803ec2d21edbe535d57fca7e0a
Nickurama <rokdoombeast@gmail.com>
2023-12-27 03:26:01 +0000

    resolved small bug

--- Integrals/main.py
+++ Integrals/main.py
@@ -2,6 +2,7 @@ import math_input
 import nc_closed
 import nc_open
 from sympy import *
+import math
 
 exit = false
 print("tip: Ctrl+C to exit!")
@@ -15,20 +16,38 @@ while exit == false:
         try:
             print(">-- Integration (Newton-Cotes Closed: n=4) --<")
             result = nc_closed.nc_closed.solve(expression, range0, range1)
-            error = nc_closed.nc_closed.calc_error(expression, range0, range1)
+            error = None
+            try:
+                error = nc_closed.nc_closed.calc_error(expression, range0, range1)
+            except Exception as e:
+                print("could not calculate error.")
+            if (math.isnan(result)):
+                raise Exception()
             print(f"result: {round(result, precision)}")
-            print(f"error: ±{round(error, precision)}")
+            try:
+                print(f"error: ±{round(error, precision)}")
+            except Exception as e:
+                pass
         except Exception as e:
             print("Newton-Cotes Closed method failed.")
+            print("Is the expression continuous in the given range?")
             print(str(e))
         try:
             print(">-- Integration (Newton-Cotes Open: n=3) --<")
             result = nc_open.nc_open.solve(expression, range0, range1)
-            error = nc_open.nc_open.calc_error(expression, range0, range1)
+            error = None
+            try:
+                error = nc_closed.nc_closed.calc_error(expression, range0, range1)
+            except Exception as e:
+                print("could not calculate error.")
             print(f"result: {round(result, precision)}")
-            print(f"error: ±{round(error, precision)}")
+            try:
+                print(f"error: ±{round(error, precision)}")
+            except Exception as e:
+                pass
         except Exception as e:
             print("Newton-Cotes Open method failed.")
+            print("Is the expression continuous in the given range?")
             print(str(e))
     except KeyboardInterrupt:
         exit = true
